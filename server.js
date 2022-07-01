284cb76efafb1b492b6ca8faaafa4a3a3709d780
Messuoh00 <houssemorsam1@gmail.com>
2022-07-01 19:47:09 +0100

    fck this

--- server.js
+++ server.js
@@ -11,14 +11,14 @@ const expresslayouts=require('express-ejs-layouts')
 
 const indexroute=require('./routes/index')
 
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
-mongoose.connect(process.env.DATABASE_URL,{usenewURLparser: true})
+// mongoose.connect(process.env.DATABASE_URL,{usenewURLparser: true})
 
-const db = mongoose.connection
+// const db = mongoose.connection
 
-db.on('error',error=>console.error('error'))
-db.on('open',()=>console.log('rahi tmchi yay connected to mongogdb:)'))
+// db.on('error',error=>console.error('error'))
+// db.on('open',()=>console.log('rahi tmchi yay connected to mongogdb:)'))
 
 app.set('view engine','ejs')
 app.set('views',__dirname+'/views')
